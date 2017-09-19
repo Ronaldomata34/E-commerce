98d9974b585f34796fe20de31e3736d10e3290f5
Ronaldo Mata <ronaldomata34@gmail.com>
2017-09-19 13:00:55 -0400

    Integrated a storage for media files Cloudinary

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+from cloudinary.models import CloudinaryField
+
 
 PLATFORM = (
     ('ps3', 'ps3'),
@@ -52,7 +54,7 @@ class Product(models.Model):
     typeofgame = models.ForeignKey(TypeOfGame, related_name='type_of_game')
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = CloudinaryField('image')
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
